2ed4f3931dfa292e5c1c62619772f04e41c68629
Antônio Roberto Silva <forevertonny@gmail.com>
2013-08-21 14:15:06 -0300

    fix typo error

--- lib/catarse_mailchimp/active_record.rb
+++ lib/catarse_mailchimp/active_record.rb
@@ -8,21 +8,21 @@ module CatarseMailchimp
     #                         subscribe_when: ->(user) { user.newsletter_changed? && user.newsletter },
     #                         unsubscribe_when: ->(user) { user.newsletter_changed? && !user.newsletter },
     #                         ubsubscribe_email: ->(user) { user.email }
-    #     
+    #
     #   end
     #
     def sync_with_mailchimp options
       self.class_eval <<-RUBY
         before_save do
-                              
+
           if options[:subscribe_when].call(self)
             CatarseMailchimp::API.subscribe(options[:subscribe_data].call(self), options[:list_id])
           end
-          
+
           if options[:unsubscribe_when].call(self)
-            CatarseMailchimp::API.unsubscribe(options[:ubsubscribe_email].call(self), options[:list_id])            
+            CatarseMailchimp::API.unsubscribe(options[:unsubscribe_email].call(self), options[:list_id])
           end
-          
+
         end
       RUBY
     end
